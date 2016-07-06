3c490f189ba9bbc5ba02c8e5b7d95ad5704a134c
Chiheb2013 <chiheb_csharp_coder@live.fr>
2016-07-06 17:33:39 +0200

    Circular frame added to primitives
    Added the circular frame as a primitive

--- Program.cs
+++ Program.cs
@@ -10,23 +10,24 @@ namespace dry
     {
         static void Main(string[] args)
         {
-            Camera camera = new Camera(ToRadians(30f), -ToRadians(10f), new Vector3D(100, 100, 0));
-            World world = new World(camera, new Viewport(640, 468));
+            Camera camera = new Camera(0, 0, Vector3D.Zero);
+            World world = new World(camera, new Viewport(640, 640));
 
             BasicMaterialProperties bmp1 = new BasicMaterialProperties(Color.Red, 0.2f, 0.8f);
             BasicMaterialProperties bmp2 = new BasicMaterialProperties(Color.Blue, 0.2f, 0.8f);
             BasicMaterialProperties bmp3 = new BasicMaterialProperties(Color.White, 0.2f, 0.8f);
             Material red = new Material(bmp1, true, world);
             Material blue = new Material(bmp2, true, world);
-            Material white = new Material(bmp3, true, true, 2.5f, world);
+            Material white = new Material(bmp3, true, true, 3f, world);
             SceneObject s1 = new SceneObject(new Sphere(50, new Vector3D(50, 0, 100)), red);
-            SceneObject s2 = new SceneObject(new Sphere(50, new Vector3D(0, 0, 100)), white);
+            SceneObject s2 = new SceneObject(new Sphere(50, new Vector3D(0, -30, 100)), white);
             SceneObject s3 = new SceneObject(new Sphere(50, new Vector3D(-50, 0, 100)), blue);
             BasicMaterialProperties bmp4 = new BasicMaterialProperties(Color.White, 0, 1);
             Material green = new Material(bmp4, true, world);
-            SceneObject pl = new SceneObject(new Plane(new Vector3D(0, 0, 160), new Vector3D(0, 1, 1)), green);
+            SceneObject pl = new SceneObject(new CircularFrame(new Vector3D(0, 0, 180), 200, 
+                new Plane(new Vector3D(0, 0, 160), new Vector3D(0, 1, 1))), green);
 
-            world.AddLight(new Light(new Vector3D(0, 200f, 100f)));
+            world.AddLight(new Light(new Vector3D(0, 60, 100f)));
             world.AddSceneObject(s1);
             world.AddSceneObject(s2);
             world.AddSceneObject(s3);
